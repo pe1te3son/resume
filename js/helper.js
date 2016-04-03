09630fc2e7ddf6ea62b8b3f578e32c404b471c96
Peter on win10 <pe1te3son@ymail.com>
2016-04-03 03:21:22 +0100

    styling footer

--- js/helper.js
+++ js/helper.js
@@ -33,7 +33,7 @@ var HTMLworkStart = '<div class="work-entry"></div>';
 var HTMLworkEmployer = '<a href="%#%">%data%';
 var HTMLworkTitle = ' - %data%</a>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
-var HTMLworkLocation = '<div class="location-text">%data%</div>';
+var HTMLworkLocation = '<div class="location-text text-right">%data%</div>';
 var HTMLworkDescription = '<p>%data%</p>';
 
 var HTMLprojectStart = '<div class="project-entry col-lg-12"></div>';
